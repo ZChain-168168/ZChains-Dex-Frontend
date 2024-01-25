c78d4c476612d2faf09a2fe44cad2f6986edd021
thanhdev2k <thanhdev.crelife@gmail.com>
2024-01-25 23:33:24 +0700

    fix domain

--- apps/web/src/components/PhishingWarningBanner/index.tsx
+++ apps/web/src/components/PhishingWarningBanner/index.tsx
@@ -36,7 +36,7 @@ const SpeechBubble = styled.div`
   }
 `
 
-const domain = 'https://snowtrace.io/'
+const domain = 'https://scan.creditsmartchain.com'
 
 const PhishingWarningBanner: React.FC<React.PropsWithChildren> = () => {
   const { t } = useTranslation()
