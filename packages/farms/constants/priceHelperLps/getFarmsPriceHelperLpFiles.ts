f44145ce83fbe7cce8db472891e5cb5a9aa274f2
thanhdev21 <thanhdev.crelife@gmail.com>
2024-06-05 12:35:13 +0700

    update ui zchain

--- packages/farms/constants/priceHelperLps/getFarmsPriceHelperLpFiles.ts
+++ packages/farms/constants/priceHelperLps/getFarmsPriceHelperLpFiles.ts
@@ -2,8 +2,6 @@ import { ChainId } from '@pancakeswap/sdk'
 import FarmsBscPriceHelper from './56'
 import FarmsBscTestnetPriceHelper from './97'
 import FarmsEthereumPriceHelper from './1'
-import FarmsGoerliPriceHelper from './5'
-import FarmsMtvPriceHelper from './10435'
 import FarmsCreditPriceHelper from './4400'
 // edit
 export const getFarmsPriceHelperLpFiles = (chainId: ChainId) => {
